a079735aba4958a372ee271acd5685aaa961592a
Srija <srija@192.168.1.218>
2021-05-25 20:02:29 +0100

    made changes in main.js

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,6 +1,7 @@
-let popped = 0;
+"use strict";
 
-document.addEventListener("click", (event) => {
+var popped = 0;
+document.addEventListener("click", function (event) {
   if (event.target.className === "balloon") {
     event.target.style.backgroundColor = "#ededed";
     event.target.textContent = "POP!";
