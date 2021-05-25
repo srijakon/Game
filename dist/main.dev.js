5d83c7a4894aceb333df35936781820e80f51309
Srija <srija@192.168.1.218>
2021-05-25 20:37:52 +0100

    Added more functions

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,12 +1,29 @@
 "use strict";
 
 var popped = 0;
-document.addEventListener("click", function (event) {
-  if (event.target.className === "balloon") {
-    event.target.style.backgroundColor = "#ededed";
-    event.target.textContent = "POP!";
-    popped++;
-    removeEvent(event);
-    checkAllPopped();
-  }
+var balloons = document.querySelectorAll(".balloon");
+balloons.forEach(function (balloon) {
+  document.addEventListener("click", function (e) {
+    if (e.target.className === "balloon") {
+      e.target.style.backgroundColor = "#ededed";
+      e.target.textContent = "POP!";
+      popped++;
+      removeEvent(e);
+      checkAllPopped();
+    }
+  });
 });
+
+function removeEvent(e) {
+  e.target.removeEventListener("click", function () {});
+}
+
+function checkAllPopped() {
+  if (popped === 11) {
+    console.log("all popped!");
+    var gallery = document.querySelector("#balloon-gallery");
+    var message = document.querySelector("#no-balloons");
+    gallery.innerHTML = "";
+    message.style.display = "block";
+  }
+}
--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 let popped = 0;
 
-document.addEventListener("click", (event) => {
-  if (event.target.className === "balloon") {
-    event.target.style.backgroundColor = "#ededed";
-    event.target.textContent = "POP!";
-    popped++;
-    removeEvent(event);
-    checkAllPopped();
-  }
+const balloons = document.querySelectorAll(".balloon");
+balloons.forEach((balloon) => {
+  document.addEventListener("click", (e) => {
+    if (e.target.className === "balloon") {
+      e.target.style.backgroundColor = "#ededed";
+      e.target.textContent = "POP!";
+      popped++;
+      removeEvent(e);
+      checkAllPopped();
+    }
+  });
 });
+function removeEvent(e) {
+  e.target.removeEventListener("click", function () {});
+}
+
+function checkAllPopped() {
+  if (popped === 11) {
+    console.log("all popped!");
+    let gallery = document.querySelector("#balloon-gallery");
+    let message = document.querySelector("#no-balloons");
+    gallery.innerHTML = "";
+    message.style.display = "block";
+  }
+}
