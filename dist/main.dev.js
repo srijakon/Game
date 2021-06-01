7a6db585860de2f25c097e2ca80a1340fa2a948e
Srija <srija@192.168.1.239>
2021-06-01 14:33:53 +0100

    changes

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -3,42 +3,48 @@
 // DECLARATION OF VARIABLES
 var popped = 0;
 var highScore = -1;
-var balloonColours = ["#5979a6", "#8882c9", "#66cc86", "#d6d585", "#a96942", "#63bbb6", "#f0753d", "#a465a1", "#b74e68", "#f14646", "#b5da5d", "#74e27a", "#a07ae6"]; // ADDED AN ARRAY OF BALLOON COLOURS
-// ADDING EVENT LISTENER FOR STARTING THE GAME -> BY SELECTING CLASSES(START, BALLOON) -> BY DISPLAYING START AS NONE AND BALLOON WILL APPEAR AS BLOCK
+var baloonColours = ["#5979a6", "#8882c9", "#66cc86", "#d6d585", "#a96942", "#63bbb6", "#f0753d", "#a465a1", "#b74e68", "#f14646", "#b5da5d", "#74e27a", "#a07ae6"]; //DECLARED AN ARRAY OF COLOURS FOR DIFFERENT COLOURED BALLOONS TO APPEAR
+// CREATED SETUPSTARTGAMEEVENTLISTENER -> ADDED AN EVENT BY SELECTING CLASS START, BALLOON AND DISPLAYING IT TO NONE AND BLOCK
 
 var setupStartGameEventListener = function setupStartGameEventListener() {
   document.querySelector(".start").addEventListener("click", function (e) {
     document.querySelector(".start").style.display = "none";
     document.querySelector(".balloon").style.display = "block";
-    startGame(); //CALLING START GAME FUNCTION
+    startGame(); //CALLING STARTGAME FUNCTION
   });
-}; // STARTGAME FUNCTION -> BALLOON EVENTS(FOR BALLOONS TO BE POPPED), RESTART EVENT(FOR RESTARTING THE GAME), SET TIMER -> TIME LIMIT FOR THE BALLOONS
+}; // CREATED STARTGAME FUNCTION -> SETUPBALLOONEVENTLISTENER -> SETUPRESTARTEVENTLISTENER -> SETTIMER
 
 
 var startGame = function startGame() {
   setupBalloonEventListener();
   setupRestartEventListener();
   setTimer();
-}; // BALLOON EVENT LISTENER -> FOR BALLOONS TO BE POPPED
+}; // SETUPBALLOONEVENTLISTENER -> ADDED AN EVENT BY SELECTING BALLOON CLASS BY CLICKING
 
 
 var setupBalloonEventListener = function setupBalloonEventListener() {
   document.querySelector(".balloon").addEventListener("click", function (event) {
-    popped++;
-    clickedBalloon(event); // CLICKED BALLOON FUNCTION
+    popped++; //
+
+    clickedBalloon(event); //CALLING CLICKEDBALLOON FUNCTION
   });
-}; // CLICKED BALLOON FUNCTION ->
+}; // CLICKEDBALLOON FUNCTION ->
 
 
 var clickedBalloon = function clickedBalloon(event) {
-  var top = Math.floor(Math.random() * 501);
-  var left = Math.floor(Math.random() * 501);
-  document.querySelector(".balloon").style.top = top + "px";
+  var top = Math.floor(Math.random() * 501); //USING RANDOM METHOD TO PLACE BALLOON BETWEEN 0 TO 500 - MARGIN TOP
+
+  var left = Math.floor(Math.random() * 501); //USING RANDOM METHOD TO PLACE BALLOON BETWEEN 0 TO 500 - MARGIN LEFT
+
+  document.querySelector(".balloon").style.top = top + "px"; //
+
   document.querySelector(".balloon").style.left = left + "px";
-  var colourIndex = Math.floor(Math.random() * 13);
-  var colour = balloonColours[colourIndex];
-  event.target.style.background = colour;
-}; // // RESTART EVENT LISTENER -> RESTARTING THE GAME BY CLICKING -> SELECTING RESTART CLASS AND ADDING A CLICK EVENT -> WHEN ITS CLICKED -> DISPLAY WILL BE NONE AND BALLOON WILL DISPLAY
+  var colourIndex = Math.floor(Math.random() * 13); // CREATED A VARIABLE WHERE RANDOM METHOD IS USED BETEEN 0 TO 13
+
+  var colour = balloonColours[colourIndex]; //CALLING ARRAY COLOURS
+
+  event.target.style.background = colour; //CHANGING THE BACKGROUND COLOR
+}; //SETUPRESTARTEVENTLISTENER FUNCTION -> SELECTING RESTART CLASS AND BALLOON CLASS AND  ADDING EVENT LISTENERS, DISPLAYING IT TO BE NONE AND INLINE-BLOCK
 
 
 var setupRestartEventListener = function setupRestartEventListener() {
@@ -47,7 +53,7 @@ var setupRestartEventListener = function setupRestartEventListener() {
     document.querySelector(".balloon").style.display = "inline-block";
     restart(); //CALLING RESTART FUNCTION
   });
-}; // RESTART FUNCTION -> DISPLAY WILL BE NONE AND COUNTDOWN WILL APPEAR
+}; // RESTART FUNCTION
 
 
 var restart = function restart() {
@@ -55,34 +61,40 @@ var restart = function restart() {
   document.querySelector(".result").style.display = "none";
   document.querySelector(".countdown").style.display = "block";
   setTimer();
-}; // // SET TIMER FUNCTION ->
+}; // SETTIMER FUNCTION
 
 
 var setTimer = function setTimer() {
-  var target = new Date();
-  target.setSeconds(target.getSeconds() + 10);
+  var target = new Date(); //CALLING DATE INBUILT METHOD
+
+  target.setSeconds(target.getSeconds() + 10); //CALLING SETSECONDS AND GETSECONDS METHOD TO SET THE TARGET TIME = 10
+
+  var timer = setInterval(function () {
+    //CREATING TIMER FUNCTION
+    var now = new Date().getTime(); //CALLING NEW DATE AND GET TIME METHODS
+
+    var distance = target - now; // TARGET TIME AND CURRENT TIME IS DISTANCE
+
+    var seconds = Math.floor(distance % (1000 * 60) / 1000); //CREATING A VARIABLE AND STORING THE SECONDS REMAINED
+
+    document.querySelector(".countdown").innerHTML = seconds + " seconds left"; // IF STATEMENTS WHETHER TO CHECK THE SCORE IS HIGHER OR NOT
+
+    if (distance < 0) {
+      document.querySelector(".balloon").style.display = "none";
+      document.querySelector(".countdown").style.display = "none";
+
+      if (popped > highScore) {
+        document.querySelector(".result").innerHTML = "Congratulations! You scored a new high of " + popped;
+        highScore = popped;
+      } else {
+        document.querySelector(".result").innerHTML = "Oh dear! Your score of " + popped + " didn't beat the high score of " + highScore;
+      }
+
+      document.querySelector(".result").style.display = "block";
+      document.querySelector(".restart").style.display = "block";
+      clearInterval(timer); //USED INBUILT TO CLEAR THE TIMER
+    }
+  }, 1000);
 };
 
-var timer = setInterval(function () {
-  var now = new Date().getTime();
-  var distance = target - now;
-  var seconds = Math.floor(distance % (1000 * 60) / 1000);
-  document.querySelector(".countdown").innerHTML = seconds + "seconds left";
-
-  if (distance < 0) {
-    document.querySelector(".balloon").style.display = "none";
-    document.querySelector(".countdown").style.display = "none";
-
-    if (popped > highScore) {
-      document.querySelector(".result").innerHTML = "Congratulations! You scored a new high of " + popped;
-      highScore = popped;
-    } else {
-      document.querySelector(".result").innerHTML = "Oh dear! Your score of " + popped + "didn't beat the high score of " + highScore;
-    }
-
-    document.querySelector(".result").style.display = "block";
-    document.querySelector(".restart").style.display = "block";
-    clearInterval(timer);
-  }
-}, 1000);
 setupStartGameEventListener();
